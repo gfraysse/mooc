6b6b8fc23da8dfd76c14e5c2d4bc971f939aa14f
Guillaume Fraysse <gfraysse.ml@gmail.com>
2018-08-07 08:35:45 +0200

    Update: 2018-08-07

--- DiscreteOptimization-Coursera/coloring/solver.py
+++ DiscreteOptimization-Coursera/coloring/solver.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Graph Coloring
+# fmYLC, ./data/gc_50_3, solver.py, Coloring Problem 1
+# IkKpq, ./data/gc_70_7, solver.py, Coloring Problem 2
+# pZOjO, ./data/gc_100_5, solver.py, Coloring Problem 3
+# XDQ31, ./data/gc_250_9, solver.py, Coloring Problem 4
+# w7hAO, ./data/gc_500_1, solver.py, Coloring Problem 5
+# tthbm, ./data/gc_1000_5, solver.py, Coloring Problem 6
+
 
 from ortools.constraint_solver import pywrapcp
+from ortools.linear_solver import pywraplp
 
 # https://github.com/google/or-tools/blob/master/examples/python/coloring_ip.py
 def ortools_alternate_solver(node_count, edge_count, edges):
     solver = pywrapcp.Solver("coloring")
     # x[i,c] = 1 means that node i is assigned color c
     nc = 10
-    x = {}
+    #x = {}
+    x = [0 for _ in range(nc*node_count)]
 
     for v in range(node_count):
         for j in range(nc):
-            x[v, j] = solver.IntVar(0, 1, 'v[%i,%i]' % (v, j))
+            #x[v, j] = solver.IntVar(0, 1, 'v[%i,%i]' % (v, j))
+            #x[nc*v+j] = solver.IntVar(0, 1, 'v[%i]' % (nc*v+j))
+            x[nc*v+j] = solver.IntVar(0, 1, 'v[%i,%i]' % (v, j))
+            #print nc*v+j
 
+    #print x
     # u[c] = 1 means that color c is used, i.e. assigned to some node
     u = [solver.IntVar(0, 1, 'u[%i]' % i) for i in range(nc)]
 
     # number of colors used, to minimize
+    solver.Add(solver.Sum(u) >= 2)
     obj = solver.Sum(u)
 
     #
@@ -25,19 +40,108 @@ def ortools_alternate_solver(node_count, edge_count, edges):
     #
     
     # each node must be assigned exactly one color
-    for i in range(node_count - 1):
+    for i in range(node_count):
+        #solver.Add(solver.Sum([x[i, c] for c in range(nc)]) == 1)
+        solver.Add(solver.Sum(x[c] for c in range(i*nc,(i+1)*nc)) == 1)
+
+    # adjacent nodes cannot be assigned the same color
+    # (and adjust to 0-based)
+    for i in range(edge_count):
+        for c in range(nc):
+            #a = x[edges[i][0], c]
+            a = x[edges[i][0]*nc + c]
+            #b = x[edges[i][1], c]
+            b = x[edges[i][1]*nc + c]
+            print "edge:"+str(edges[i])+" a="+str(a)+" b="+str(b)+" u["+str(c)+"]="+str(u[c])
+            solver.Add(a + b <= u[c])
+
+    # objective
+    objective = solver.Minimize(obj, 1)
+
+    db = solver.Phase(x,
+                      solver.CHOOSE_FIRST_UNBOUND,
+                      solver.ASSIGN_RANDOM_VALUE)
+
+    collector = solver.LastSolutionCollector()
+    collector.AddObjective(obj)
+    #
+    # solution
+    #
+    solver.Solve(db,[objective, collector])
+    
+    #print 'number of colors:', int(solver.Objective().Value())
+    #print 'colors used:', [int(u[i].Value()) for i in range(nc)]
+
+    # for v in range(node_count):
+    #     print 'v%i' % v, ' color '
+    #     for c in range(nc):
+    #         if int(x[v, c].Value()) == 1:
+    #             print(c)
+
+    # Iterates through the solutions, displaying each.
+    num_solutions = 0
+    num_colors = 0
+    solution = []
+    while solver.NextSolution():
+        solution = []
+        print x
+
+        # Displays the solution just computed.
+        for v in range(node_count):
+            for c in range(nc):
+                #print(node_color[i].Value())
+                #if num_solutions == 0:
+                solution.append(x[v, c].Value())
+                
+        num_colors = max(solution)+1
+        num_solutions += 1
+                #print num_colors, solution
+
+    solver.EndSearch()
+    print "Solutions found:", num_solutions
+    print "Time:", solver.WallTime(), "ms"
+    
+    return num_colors, solution
+
+# https://github.com/google/or-tools/blob/master/examples/python/coloring_ip.py
+def ortools_linear_solver(node_count, edge_count, edges):
+    solver = pywraplp.Solver('CoinsGridGLPK',
+                             pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+    # x[i,c] = 1 means that node i is assigned color c
+    nc = 10
+    x = {}
+
+    for v in range(node_count):
+        for j in range(nc):
+            x[v, j] = solver.IntVar(0, 1, 'v[%i,%i]' % (v, j))
+            #print nc*v+j
+
+    #print x
+    # u[c] = 1 means that color c is used, i.e. assigned to some node
+    u = [solver.IntVar(0, 1, 'u[%i]' % i) for i in range(nc)]
+
+    # number of colors used, to minimize
+    solver.Add(solver.Sum(u) >= 2)
+    obj = solver.Sum(u)
+
+    #
+    # constraints
+    #
+    
+    # each node must be assigned exactly one color
+    for i in range(node_count):
         solver.Add(solver.Sum([x[i, c] for c in range(nc)]) == 1)
 
     # adjacent nodes cannot be assigned the same color
     # (and adjust to 0-based)
-    for i in range(edge_count - 1):
+    for i in range(edge_count):
         for c in range(nc):
             a = x[edges[i][0], c]
             b = x[edges[i][1], c]
             solver.Add(a + b <= u[c])
 
     # objective
-    objective = solver.Minimize(obj, 1)
+    objective = solver.Minimize(obj)
 
     #
     # solution
@@ -52,27 +156,6 @@ def ortools_alternate_solver(node_count, edge_count, edges):
         for c in range(nc):
             if int(x[v, c].SolutionValue()) == 1:
                 print(c)
-
-    # # Iterates through the solutions, displaying each.
-    # num_solutions = 0
-    # num_colors = 0
-    # solution = []
-    # while solver.NextSolution():
-    #     solution = []
-
-    #     # Displays the solution just computed.
-    #     for i in range(node_count):
-    #         #print(node_color[i].Value())
-    #         #if num_solutions == 0:
-    #         solution.append(node_color[i].Value())
-
-    #     num_colors = max(solution)+1
-    #     num_solutions += 1
-    #     #print num_colors, solution
-
-    # solver.EndSearch()
-    # print "Solutions found:", num_solutions
-    # print "Time:", solver.WallTime(), "ms"
     
     return num_colors, solution
 
@@ -82,21 +165,106 @@ def ortools_solver(node_count, edge_count, edges):
     # Creates the variables.
     nodes = [solver.IntVar(0, node_count - 1, "node%i" % i) for i in range(node_count)]
     neighbours = [[] for _ in range(node_count)]
-    node_color = [solver.IntVar(0, 20, "node_color%i" % i) for i in range(node_count)]
+    
+    nc = 10
+    node_color = [0 for _ in range(nc*node_count)]
 
+    for v in range(node_count):
+        for j in range(nc):
+            node_color[nc*v+j] = solver.IntVar(0, 1, 'v[%i,%i]' % (v, j))
+            #print nc*v+j
+
+    # each node must be assigned exactly one color
+    for i in range(node_count):
+        solver.Add(solver.Sum(node_color[c] for c in range(i*nc,(i+1)*nc)) == 1)
+        #print i,range(i*nc,(i+1)*nc)
+        
+    # u[c] = 1 means that color c is used, i.e. assigned to some node
+    u = [solver.IntVar(0, 1, 'u[%i]' % i) for i in range(nc)]
+
+    #node_color = [solver.IntVar(0, nc, "node_color%i" % i) for i in range(node_count)] # color of each node
+    lb_color_total_nodes = [0 for _ in range(node_count)] # number of nodes for each color lower bound
+    ub_color_total_nodes = [node_count for _ in range(node_count)] # number of nodes for each color upper bound
     # Creates the constraints.    
-    # All rows must be different.
     for edge in edges:
-        solver.Add(node_color[edge[0]] != node_color[edge[1]])
         neighbours[edge[0]].append(node_color[edge[1]]) #solver.IntVar(edge[1],edge[1]))
         neighbours[edge[1]].append(node_color[edge[0]]) #solver.IntVar(edge[0],edge[0]))
+        for c in range(nc):
+            #     solver.Add(node_color[edge[0]*nc+c] != node_color[edge[1]*nc+c])
+            a = node_color[edge[0] * nc + c]
+            b = node_color[edge[1] * nc + c]
+            #a = solver.Add(solver.Sum(node_color[edge[0] * nc:(edge[0]+1) * nc])+solver.Sum(node_color[edge[1] * nc:(edge[1]+1) * nc]) <= u[c])
+            solver.Add(a + b <= u[c])
 
+        
+    # for i in range(len(neighbours)):
+    #     lb_color_total_nodes[node_color[i]] += len(neighbours[i])
+    #     ub_color_total_nodes[node_color[i]] -= len(neighbours[i])
+
+
+    # for i in range(node_count):
+    #     solver.Add(lb_color_total_nodes[i] < ub_color_total_nodes[i])
+    #     solver.Add(solver.Sum[i] < ub_color_total_nodes[i])
+    #     solver.Add(solver.Sum[i] > lb_color_total_nodes[i])
     # Absurd neighbors of a node can be of the same color...
     # for n in neighbours:
     #     print n
         #solver.Add(solver.AllDifferent(n))
     # Trying to break symmetries
     #solver.Add(node_color[edge[0]] <= node_color[edge[1]])
+        
+    # Find the solution that minimizes the number of colors of the graph
+    obj_var = solver.Sum(u)
+    #obj_var = solver.Max(node_color)
+    objective_monitor = solver.Minimize(obj_var, 1)
+                               
+    db = solver.Phase(node_color,
+                      solver.CHOOSE_FIRST_UNBOUND,
+                      solver.ASSIGN_RANDOM_VALUE)
+    
+    collector = solver.LastSolutionCollector()
+
+    solution = []
+    # Add the interesting variables to the SolutionCollector.
+    collector.Add(node_color)
+    collector.AddObjective(obj_var)
+    
+    solver.NewSearch(db, [objective_monitor, collector])
+    
+    # Iterates through the solutions, displaying each.
+    num_solutions = 0
+    num_colors = 0
+    while solver.NextSolution():
+        solution = []
+
+        # Displays the solution just computed.
+        for i in range(node_count):
+            #print(node_color[i].Value())
+            #if num_solutions == 0:
+            solution.append(node_color[i].Value())
+
+        num_colors = max(solution)+1
+        num_solutions += 1
+        #print num_colors, solution
+
+    solver.EndSearch()
+    print "Solutions found:", num_solutions
+    print "Time:", solver.WallTime(), "ms"
+    
+    return num_colors, solution
+
+def ortools_solver_v1(node_count, edge_count, edges):
+    solver = pywrapcp.Solver("coloring")
+    
+    # Creates the variables.
+    nodes = [solver.IntVar(0, node_count - 1, "node%i" % i) for i in range(node_count)]
+    neighbours = [[] for _ in range(node_count)]
+    
+    node_color = [solver.IntVar(0, 20, "node_color%i" % i) for i in range(node_count)]
+
+    # Creates the constraints.    
+    for edge in edges:
+        solver.Add(node_color[edge[0]] != node_color[edge[1]])        
         
     # Find the solution that minimizes the number of colors of the graph
     obj_var = solver.Max(node_color)
@@ -156,7 +324,9 @@ def solve_it(input_data):
     # build a trivial solution
     # every node has its own color
     #solution = range(0, node_count)
-    num_colors, solution = ortools_alternate_solver(node_count, edge_count, edges)
+    #num_colors, solution = ortools_linear_solver(node_count, edge_count, edges)
+    #num_colors, solution = ortools_alternate_solver(node_count, edge_count, edges)
+    num_colors, solution = ortools_solver_v1(node_count, edge_count, edges)
 
 
     # prepare the solution in the specified output format
