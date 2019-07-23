bc976e00a262a7f7ac6b2ab35620ce5a589830ea
Guillaume Fraysse <gfraysse.ml@gmail.com>
2019-07-23 14:27:31 +0200

    ADD: answers to Week2 Ex1

--- MachineLearning-Coursera/machine-learning-ex1/ex1/gradientDescent.m
+++ MachineLearning-Coursera/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,9 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    S = sum(theta' .* X, 2); % 2 indicated to sum each row
+    P = (S - y) .* X;
+    theta = theta - (sum(P) .* (alpha / m))';    
 
 
 
